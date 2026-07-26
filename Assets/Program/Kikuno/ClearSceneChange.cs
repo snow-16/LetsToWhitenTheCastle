15e07d60ed748fb1a11a9a2c237e377df2824b8a
kikunosatoshi <kikuno.satos@gmail.com>
2026-07-27 01:28:37 +0900

    [Add]StartScene、ClearScene及び、ClearSceneChange.csの追加

---
+++ Assets/Program/Kikuno/ClearSceneChange.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClearSceneChange : MonoBehaviour
+{
+        public void LoadClearScene()
+    {
+        SceneManager.LoadScene("ClearScene");
+    }
+}
