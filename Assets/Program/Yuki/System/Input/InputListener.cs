39516b729f6d2067a33a416caac9f92d204bca50
AizawaYuki <imagination1159@gmail.com>
2026-07-27 13:13:49 +0900

    [add]入力の処理優先度を追加&InputListenerをインスペクター拡張

--- Assets/Program/Yuki/System/Input/InputListener.cs
+++ Assets/Program/Yuki/System/Input/InputListener.cs
@@ -4,38 +4,89 @@ using System.Linq;
 using UniRx;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class InputListener : MonoBehaviour
 {
     [SerializeField]
     private KeyConfigData _keyConfigData;
     [SerializeField]
-    private List<ObservableInput> _observableInputs;
+    private InputType _listeningType;
+    public InputType ListeningType { get => _listeningType; set => _listeningType = value; }
+    [SerializeField]
+    private List<ObservableInput> _observableInputs = new();
+    public List<ObservableInput> ObservableInputs { get => _observableInputs; set => _observableInputs = value; }
 
     void Start()
     {
-        _observableInputs.ForEach(observer =>
+        for(int i = 0; i < _observableInputs.Count; i++)
+        {
+            if((_listeningType & (InputType)(1 << i)) > 0)
             {
-                var input = _keyConfigData.KeyConfigs[(int)observer.type].input;
+                SubscribeInputs((InputType)(1 << i));
+            }
+        }
+    }
 
-                input.Enable();
-                Observable.EveryUpdate()
-                .Where(_ => input.IsPressed())
-                .Where(_ => observer.inputFilters.All(item => item.inputFilter.WhenCanInput()))
-                .Subscribe(_ =>
+    private void SubscribeInputs(InputType inputType)
+    {
+        var input = _keyConfigData.KeyConfigs[(int)Mathf.Log((int)inputType, 2)].input;
+        input.Enable();
+
+        Observable.EveryUpdate()
+        .Where(_ => input.WasPressedThisFrame() || input.WasReleasedThisFrame() || input.IsPressed())
+        .Subscribe(_ =>
+            {
+                foreach(var inputSetting in _observableInputs[(int)Mathf.Log((int)inputType, 2)].inputSettings)
+                {
+                    if(FireInput(inputSetting, input))
                     {
-                        observer.action.Invoke(input.ReadValue<float>());
+                        inputSetting.action.Invoke(input.ReadValue<float>());
+                        break;
                     }
-                ).AddTo(this);
+                };
             }
         );
     }
 
+    private bool FireInput(InputSetting inputSetting, InputAction input)
+    {
+        if(inputSetting.inputFilters.Count == 0 || inputSetting.inputFilters.All(item => item.inputFilter.WhenCanInput()))
+        {
+            switch(inputSetting.state)
+            {
+                case InputState.Pressed :
+                {
+                    return input.WasPressedThisFrame();
+                }
+                case InputState.Pressing :
+                {
+                    return input.IsPressed();
+                }
+                case InputState.Released :
+                {
+                    return input.WasReleasedThisFrame();
+                }
+                case InputState.PressOrReleased :
+                {
+                    return input.WasPressedThisFrame() || input.WasReleasedThisFrame();
+                }
+            }
+        }
+
+        return false;
+    }
+
     [Serializable]
-    private struct ObservableInput
+    public struct ObservableInput
+    {
+        public List<InputSetting> inputSettings;
+    }
+
+    [Serializable]
+    public struct InputSetting
     {
         public string name;
-        public InputType type;
         public InputState state;
         public List<InputFilter> inputFilters;
         public ValueEvent action;
@@ -44,7 +95,7 @@ public class InputListener : MonoBehaviour
     }
 
     [Serializable]
-    private struct InputFilter
+    public struct InputFilter
     {
         [SerializeReference, SubclassSelector]
         public IInputFilter inputFilter;
