57bd091bde8b0af404ee5767babe56494c2303df
shinogon2007 <shinogon2007@gmail.com>
2026-07-27 21:24:40 +0900

    説明文の追加とbossの攻撃時のバレットが地形に接触した際にdestroyするようにした

--- Assets/Program/Sino/ATK/LittleATKList.cs
+++ Assets/Program/Sino/ATK/LittleATKList.cs
@@ -7,6 +7,7 @@ public class LittleATKList : MonoBehaviour
     [SerializeField] GameObject _gunPrefab;
     [SerializeField] GameObject _firePrefab;
     [SerializeField] float _moveSpeed;
+    int _movementAttackDamge;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,39 +21,43 @@ public class LittleATKList : MonoBehaviour
 
     }
 
-    public IEnumerator Arrow()
+    public IEnumerator Arrow()//bossの座標に矢を作成しプレイヤーに向かって飛ばす
     {
         GameObject arrow = Instantiate(_arrowPrefab, transform.position, Quaternion.identity);
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         Vector2 direction = player.transform.position - arrow.transform.position;
         Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+        BulletHitAndDestroySys damageSys = arrow.GetComponent<BulletHitAndDestroySys>();
         rb.linearVelocity = direction * _moveSpeed;
         yield return null;
     }
 
-    public IEnumerator Gun()
+    public IEnumerator Gun()//bossの座標に弾を作成しプレイヤーに向かって飛ばす
     {
         GameObject gun = Instantiate(_arrowPrefab, transform.position, Quaternion.identity);
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         Vector2 direction = player.transform.position - gun.transform.position;
         Rigidbody2D rb = gun.GetComponent<Rigidbody2D>();
+        BulletHitAndDestroySys damageSys = gun.GetComponent<BulletHitAndDestroySys>();
         rb.linearVelocity = direction * _moveSpeed * 1.5f;
         yield return null;
     }
 
-    public IEnumerator FireDast()
+    public IEnumerator FireDast()//bossの座標に火の粉を作成しプレイヤーに向かって飛ばす
     {
         GameObject fireDast = Instantiate(_arrowPrefab, transform.position, Quaternion.identity);
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         Vector2 direction = player.transform.position - fireDast.transform.position;
         Rigidbody2D rb = fireDast.GetComponent<Rigidbody2D>();
+        BulletHitAndDestroySys damageSys = fireDast.GetComponent<BulletHitAndDestroySys>();
         rb.linearVelocity = direction * _moveSpeed * 0.8f;
         fireDast.transform.localScale *= 2;
         yield return null;
     }
 
-    public void RandamLTLATK()
+    public void RandamLTLATK(int damage)
     {
+        _movementAttackDamge = damage;
         int AttackNum = Random.Range(0, 3);
         if (AttackNum == 0)
         {
