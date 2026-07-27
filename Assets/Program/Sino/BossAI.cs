32e5da7ae0d6a77697aab8a2ecdbf71c2b0c84e4
shinogon2007 <shinogon2007@gmail.com>
2026-07-27 19:37:59 +0900

    攻撃時アニメーション再生の追加とbossAIの利便性の最適化 アニメーションはステイトの名前を入れて

--- Assets/Program/Sino/BossAI.cs
+++ Assets/Program/Sino/BossAI.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 
 public class BossAI : MonoBehaviour
 {
+    Animator _anim;//アニメーション
     [Tooltip("攻撃のクールダウン")]
     [SerializeField] float _attackCoolDownTime; //攻撃のクールダウン
     bool _isCoolDown = false;//クールダウンが発生しているか否か
@@ -19,12 +20,18 @@ public class BossAI : MonoBehaviour
     [SerializeField] int _attackCount2To3 = 0;//中攻撃を何回行うと大攻撃を行うか
     int _nowAttackCount2To3;//今何度目の中攻撃か
     [Header("攻撃時の処理")]
+    [Tooltip("小攻撃のアニメーションの名前")]
+    public string _attackEvent1AnimState1;//小攻撃時のアニメーションのステート名
     [Tooltip("小攻撃の処理")]
-    [SerializeField] UnityEvent _attackEventLange1 = null;
+    [SerializeField] UnityEvent _attackEvent1 = null;//小攻撃時の処理
+    [Tooltip("中攻撃のアニメーションの名前")]
+    public string _attackEvent1AnimState2;//中攻撃のアニメーションのステート名
     [Tooltip("中攻撃の処理")]
-    [SerializeField] UnityEvent _attackEventLange2 = null;
+    [SerializeField] UnityEvent _attackEvent2 = null;//中攻撃の処理
+    [Tooltip("大攻撃のアニメーションの名前")]
+    public string _attackEvent1AnimState3;//大攻撃のアニメーションステート名
     [Tooltip("大攻撃の処理")]
-    [SerializeField] UnityEvent _attackEventLange3 = null;
+    [SerializeField] UnityEvent _attackEvent3 = null;//大攻撃の処理
     [Header("HPによる攻撃時の処理")]
     [Tooltip("小攻撃")]
     [SerializeField] UnityEvent _attackEventHP1 = null;
@@ -34,7 +41,7 @@ public class BossAI : MonoBehaviour
     [SerializeField] UnityEvent _attackEventHP3 = null;
     void Start()
     {
-
+        _anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -49,11 +56,15 @@ public class BossAI : MonoBehaviour
 
     IEnumerator StartAttack()
     {
+        float animTime;
         if (_nowAttackCount2To3 == _attackCount2To3)
         {
             _nowAttackCount2To3 = 0;
-            _attackEventLange3.Invoke();
-            yield return new WaitUntil(() => _isActing);
+            _anim.Play(_attackEvent1AnimState3);
+            yield return null;
+            animTime = _anim.GetCurrentAnimatorStateInfo(0).length;
+            yield return new WaitForSeconds(animTime);
+            _attackEvent3.Invoke();
             Debug.Log("3攻撃");
             _isCoolDown = true;
             yield return new WaitForSeconds(_attackCoolDownTime);
@@ -62,8 +73,11 @@ public class BossAI : MonoBehaviour
         else if (_nowAttackCount1To2 == _attackCount1To2)
         {
             _nowAttackCount1To2 = 0;
-            _attackEventLange2.Invoke();
-            yield return new WaitUntil(() => _isActing);
+            _anim.Play(_attackEvent1AnimState2);
+            yield return null;
+            animTime = _anim.GetCurrentAnimatorStateInfo(0).length;
+            yield return new WaitForSeconds(animTime);
+            _attackEvent2.Invoke();
             Debug.Log("2攻撃");
             _isCoolDown = true;
             yield return new WaitForSeconds(_attackCoolDownTime);
@@ -72,8 +86,11 @@ public class BossAI : MonoBehaviour
         }
         else if (_nowAttackCount1To2 != _attackCount1To2)
         {
-            _attackEventLange1.Invoke();
-            yield return new WaitUntil(() => _isActing);
+            _attackEvent1.Invoke();
+            _anim.Play(_attackEvent1AnimState1);
+            yield return null;
+            animTime = _anim.GetCurrentAnimatorStateInfo(0).length;
+            yield return new WaitForSeconds(animTime);
             Debug.Log("1攻撃");
             _isCoolDown = true;
             yield return new WaitForSeconds(_attackCoolDownTime);
