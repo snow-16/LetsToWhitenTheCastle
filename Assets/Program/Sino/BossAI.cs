add149ae6fc49c0ada06bedb4764b38a05394a41
shinogon2007 <shinogon2007@gmail.com>
2026-07-28 11:38:09 +0900

    指定した体力の割合を下回った場合敵の攻撃が変化するようにした

--- Assets/Program/Sino/BossAI.cs
+++ Assets/Program/Sino/BossAI.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 public class BossAI : MonoBehaviour
 {
     Animator _anim;//アニメーション
+    LifeSystem _lifeSystem;//ボスのライフシステム
     [Tooltip("攻撃のクールダウン")]
     [SerializeField] float _attackCoolDownTime; //攻撃のクールダウン
     bool _isCoolDown = false;//クールダウンが発生しているか否か
@@ -33,6 +34,7 @@ public class BossAI : MonoBehaviour
     [Tooltip("大攻撃の処理")]
     [SerializeField] UnityEvent _attackEvent3 = null;//大攻撃の処理
     [Header("HPによる攻撃時の処理")]
+    [SerializeField] float _attackChangePercent;//何パーセントで攻撃が変化するか
     [Tooltip("小攻撃")]
     [SerializeField] UnityEvent _attackEventHP1 = null;
     [Tooltip("中攻撃")]
@@ -42,61 +44,66 @@ public class BossAI : MonoBehaviour
     void Start()
     {
         _anim = GetComponent<Animator>();
+        _lifeSystem = GetComponent<LifeSystem>();
     }
 
     void Update()
     {
-        if(!_isCoolDown)StartCoroutine(StartAttack());
+        if (!_isCoolDown) StartCoroutine(StartAttack());
     }
 
     IEnumerator StartHpAttack()
     {
+        if ((_lifeSystem._hp / _lifeSystem._maxHP) * 100 >=  _attackChangePercent)
+        {
+            StartCoroutine(StartAttack());
+        }
         yield return null;
     }
 
-    IEnumerator StartAttack()
-    {
-        float animTime;
-        if (_nowAttackCount2To3 == _attackCount2To3)//中攻撃を一定回数行うと大攻撃を行う
-        {
-            _isCoolDown = true;
-            _nowAttackCount2To3 = 0;
-            _anim.Play(_attackEvent1AnimState3);
-            yield return null;
-            animTime = _anim.GetCurrentAnimatorStateInfo(0).length;
-            yield return new WaitForSeconds(animTime);
-            _attackEvent3.Invoke();
-            Debug.Log("3攻撃");
-            yield return new WaitForSeconds(_attackCoolDownTime);
-            _isCoolDown = false;
-        }
-        else if (_nowAttackCount1To2 == _attackCount1To2) //一定回数小攻撃を行うと中攻撃を行う
-        {
-            _isCoolDown = true;
-            _nowAttackCount1To2 = 0;
-            _anim.Play(_attackEvent1AnimState2);
-            yield return null;
-            animTime = _anim.GetCurrentAnimatorStateInfo(0).length;
-            yield return new WaitForSeconds(animTime);
-            _attackEvent2.Invoke();
-            Debug.Log("2攻撃");
-            yield return new WaitForSeconds(_attackCoolDownTime);
-            _isCoolDown = false;
-            _nowAttackCount2To3++;
-        }
-        else if (_nowAttackCount1To2 != _attackCount1To2) //小攻撃を行うのと小攻撃の回数を記録する
-        {
-            _isCoolDown = true;
-            _attackEvent1.Invoke();
-            _anim.Play(_attackEvent1AnimState1);
-            yield return null;
-            animTime = _anim.GetCurrentAnimatorStateInfo(0).length;
-            yield return new WaitForSeconds(animTime);
-            Debug.Log("1攻撃");
-            yield return new WaitForSeconds(_attackCoolDownTime);
-            _isCoolDown = false;
-            _nowAttackCount1To2++;
-        }
 
-    }
+        IEnumerator StartAttack()
+        {
+            float animTime;
+            if (_nowAttackCount2To3 == _attackCount2To3)//中攻撃を一定回数行うと大攻撃を行う
+            {
+                _isCoolDown = true;
+                _nowAttackCount2To3 = 0;
+                _anim.Play(_attackEvent1AnimState3);
+                yield return null;
+                animTime = _anim.GetCurrentAnimatorStateInfo(0).length;
+                yield return new WaitForSeconds(animTime);
+                _attackEvent3.Invoke();
+                Debug.Log("3攻撃");
+                yield return new WaitForSeconds(_attackCoolDownTime);
+                _isCoolDown = false;
+            }
+            else if (_nowAttackCount1To2 == _attackCount1To2) //一定回数小攻撃を行うと中攻撃を行う
+            {
+                _isCoolDown = true;
+                _nowAttackCount1To2 = 0;
+                _anim.Play(_attackEvent1AnimState2);
+                yield return null;
+                animTime = _anim.GetCurrentAnimatorStateInfo(0).length;
+                yield return new WaitForSeconds(animTime);
+                _attackEvent2.Invoke();
+                Debug.Log("2攻撃");
+                yield return new WaitForSeconds(_attackCoolDownTime);
+                _isCoolDown = false;
+                _nowAttackCount2To3++;
+            }
+            else if (_nowAttackCount1To2 != _attackCount1To2) //小攻撃を行うのと小攻撃の回数を記録する
+            {
+                _isCoolDown = true;
+                _attackEvent1.Invoke();
+                _anim.Play(_attackEvent1AnimState1);
+                yield return null;
+                animTime = _anim.GetCurrentAnimatorStateInfo(0).length;
+                yield return new WaitForSeconds(animTime);
+                Debug.Log("1攻撃");
+                yield return new WaitForSeconds(_attackCoolDownTime);
+                _isCoolDown = false;
+                _nowAttackCount1To2++;
+            }
+        }
 }
