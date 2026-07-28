c19809dde38c4b0624eb857ef693e925dd2c311f
shinogon2007 <shinogon2007@gmail.com>
2026-07-28 15:12:09 +0900

    インスペクター上から速度の倍率を変更できるようにした ATKListの利便性向上 矢を複数本生成し同時発射するようにした

--- Assets/Program/Sino/ATK/ATKList.cs
+++ Assets/Program/Sino/ATK/ATKList.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class ATKList : MonoBehaviour
 {
-    [SerializeField] GameObject _arrowPrefab;
-    [SerializeField] GameObject _gunPrefab;
-    [SerializeField] GameObject _fireDastPrefab;
-    [SerializeField] GameObject _canonBalletPrefab;
-    [SerializeField] GameObject _fallStonePrefab;
-    [SerializeField] float _moveSpeed;
+    [Header("対応するプレファブを設定してください")]
+    [SerializeField] GameObject _arrowPrefab;//矢のプレファブ
+    [SerializeField] GameObject _gunPrefab;//弾のプレファブ
+    [SerializeField] GameObject _fireDastPrefab;//火の粉のプレファブ
+    [SerializeField] GameObject _canonBalletPrefab;//砲弾のプレファブ
+    [SerializeField] GameObject _fallStonePrefab;//落石のプレファブ
+    [Header("共通設定")]
+    [Tooltip("生成されたオブジェクトの移動速度")]
+    [SerializeField] float _moveSpeed = 6;
+    [Header("矢の設定")]
+    [Tooltip("一度に生成される矢の本数")]
+    [SerializeField] int _createArrowSam = 3;
+    [Tooltip("生成された矢どうしのy軸の距離")]
+    [SerializeField] float _createArrowPosPlasY = 1;
+    [Header("弾丸の設定")]
+    [Tooltip("弾丸の移動速度にかかる_moveSpeed倍率")]
+    [SerializeField] float _gunMoveSpeedMagnification = 1.5f;
+    [Header("火の粉の設定")]
+    [Tooltip("生成される火の粉の数")]
     [SerializeField] float _createFireDastSam = 6;
-    [SerializeField] float _restCreateFireDastTime= 0.3f;
-    [SerializeField] float _hitPredictionTime;
-    [SerializeField] float _createStonePosYtoPlayer;
+    [Tooltip("火の粉の移動速度にかかる_moveSpeed倍率")]
+    [SerializeField] float _fireDastMoveSpeedMagnification = 0.7f;
+    [Tooltip("火の粉が再度生成されるまでの時間")]
+    [SerializeField] float _restCreateFireDastTime = 0.3f;
+    [Header("砲弾の設定")]
+    [Tooltip("着弾するまでの時間")]
+    [SerializeField] float _hitPredictionTime = 2;
+    [Header("落石の設定")]
+    [Tooltip("プレイヤーのy軸上に生成される高さ")]
+    [SerializeField] float _createStonePosYtoPlayer = 6;
     int _movementAttackDamge;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -30,14 +50,20 @@ public class ATKList : MonoBehaviour
 
     public IEnumerator Arrow(int damage)//bossの座標に矢を作成しプレイヤーに向かって飛ばす
     {
-        GameObject arrow = Instantiate(_arrowPrefab, transform.position, Quaternion.identity);
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        Vector2 direction = player.transform.position - arrow.transform.position;
-        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
-        BulletHitAndDestroySys Damage = arrow.GetComponent<BulletHitAndDestroySys>();
-        Damage._damage = damage;
-        rb.linearVelocity = direction.normalized * _moveSpeed;
-        yield return null;
+        float createArrowPosY = 0;
+        for (int i =0; i < _createArrowSam; i++)
+        {
+            GameObject arrow = Instantiate(_arrowPrefab, transform.position + Vector3.up * createArrowPosY, Quaternion.identity);
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            Vector2 direction = player.transform.position - arrow.transform.position;
+            Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+            BulletHitAndDestroySys Damage = arrow.GetComponent<BulletHitAndDestroySys>();
+            Damage._damage = damage;
+            rb.linearVelocity = direction.normalized * _moveSpeed;
+            createArrowPosY += _createArrowPosPlasY;
+            yield return null;
+        }
+        
     }
 
     public IEnumerator Gun(int damede)//bossの座標に弾を作成しプレイヤーに向かって飛ばす
@@ -48,7 +74,7 @@ public class ATKList : MonoBehaviour
         Rigidbody2D rb = gun.GetComponent<Rigidbody2D>();
         BulletHitAndDestroySys Damage = gun.GetComponent<BulletHitAndDestroySys>();
         Damage._damage = damede;
-        rb.linearVelocity = direction.normalized * _moveSpeed * 1.5f;
+        rb.linearVelocity = direction.normalized * _moveSpeed * _gunMoveSpeedMagnification;
         yield return null;
     }
 
