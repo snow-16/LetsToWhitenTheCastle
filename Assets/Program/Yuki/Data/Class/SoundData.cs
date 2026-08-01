336e366bd0c7c9748c147691c939394807eb97f2
AizawaYuki <imagination1159@gmail.com>
2026-08-01 10:03:15 +0900

    [add]音源設定項目&音源指定追加

--- Assets/Program/Yuki/Data/Class/SoundData.cs
+++ Assets/Program/Yuki/Data/Class/SoundData.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SoundData", menuName = "Scriptable Objects/SoundData")]
 public class SoundData : ScriptableObject
 {
-    
+    /// <summary> 各種BGMのデータ </summary>
+    [SerializeField]
+    [Tooltip("BGMの種類とサウンドデータです。")]
+    private List<AudioClip> _bgmList = new();
+    /// <summary> 各種BGMのデータ </summary>
+    public List<AudioClip> BGMList => _bgmList;
+
+    /// <summary> 各種SEのデータ </summary>
+    [SerializeField]
+    [Tooltip("SEの種類とサウンドデータです。")]
+    private List<AudioClip> _seList = new();
+    /// <summary> 各種SEのデータ </summary>
+    public List<AudioClip> SEList => _seList;
 }
---
+++ Assets/Program/Yuki/Enum/BGMType.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// BGMの種類
+/// </summary>
+public enum BGMType
+{
+    Title,
+    Battle,
+    Clear,
+    GameOver,
+}
